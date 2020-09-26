3e6ca086a74f91d52ef4c27f72f1c9a77a4db05b
Baher Abdelfatah <36481333+BaherAbdelfatah@users.noreply.github.com>
2020-09-26 23:39:14 +0200

    Add Week 1 Solutions

---
+++ Week 1/Solutions/trie/trie.cpp
@@ -0,0 +1,58 @@
+#include <string>
+#include <iostream>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+typedef map<char, int> edges;
+typedef vector<edges> trie;
+
+trie build_trie(vector<string> & patterns) {
+  trie t;
+  edges root;
+  // write your code here
+  t.emplace_back(root);
+  int nodeNum=0;
+  for (string pattern : patterns) 
+  {
+	  int currentNode=0;
+	  for(int i=0;i<pattern.length();i++)
+	  {
+		  char currentSymbol = pattern[i];
+		  auto edgeIt=t[currentNode].find(currentSymbol);
+		  if(edgeIt != t[currentNode].end())
+		  {
+			  currentNode=edgeIt->second;
+		  }
+		  else
+		  {
+			t[currentNode][currentSymbol]=++nodeNum;
+			edges temp;
+			t.push_back(temp);
+			currentNode=nodeNum;
+		  }
+	  }
+  }
+  return t;
+}
+
+int main() {
+  size_t n;
+  std::cin >> n;
+  vector<string> patterns;
+  for (size_t i = 0; i < n; i++) {
+    string s;
+    std::cin >> s;
+    patterns.push_back(s);
+  }
+
+  trie t = build_trie(patterns);
+  for (size_t i = 0; i < t.size(); ++i) {
+    for (const auto & j : t[i]) {
+      std::cout << i << "->" << j.second << ":" << j.first << "\n";
+    }
+  }
+
+  return 0;
+}
---
+++ Week 1/Solutions/trie_matching_extended/trie_matching_extended.cpp
@@ -0,0 +1,168 @@
+#include <algorithm>
+#include <cassert>
+#include <cstdio>
+#include <iostream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int const Letters =    4;
+int const NA      =   -1;
+
+struct Node
+{
+	int next [Letters];
+	bool patternEnd;
+
+	Node ()
+	{
+		fill (next, next + Letters, NA);
+		patternEnd = false;
+	}
+};
+
+int letterToIndex (char letter)
+{
+	switch (letter)
+	{
+		case 'A': return 0; break;
+		case 'C': return 1; break;
+		case 'G': return 2; break;
+		case 'T': return 3; break;
+		default: assert (false); return -1;
+	}
+}
+
+typedef vector<Node> trie;
+
+
+
+int prefixTrieMatching(string text, trie &t)
+{
+	char currentSymbol = text[0];
+	Node currentNode = t[0];
+	unsigned int letterId = 0;
+	int currentSymToInd = letterToIndex(currentSymbol);
+	while(true)
+	{
+		if(currentNode.patternEnd)
+		{
+			return 1;
+		}
+		else if(currentNode.next[currentSymToInd] != NA)
+		{
+			currentNode = t[currentNode.next[currentSymToInd]];
+			if (letterId < text.size() - 1)
+			{
+				currentSymbol = text[++letterId];
+				currentSymToInd = letterToIndex(currentSymbol);
+			}
+			else
+			{
+				if(currentNode.patternEnd)
+				{
+					return 1;
+				}
+				else
+				{
+					return 0;
+				}
+			}
+
+		}
+		else
+		{
+			return 0;
+		}
+	}
+
+}
+
+trie build_trie(vector<string> & patterns) {
+  trie t;
+  Node root;
+  t.emplace_back(root);
+  int nodeNum=0;
+  for (string pattern : patterns) 
+  {
+	  int currentNode=0;
+	  for(int i=0;i<pattern.length();i++)
+	  {
+		char currentSymbol = pattern[i];
+		int currentIndex = letterToIndex(currentSymbol);
+		int nextNode = t[currentNode].next[currentIndex];
+		if(nextNode != NA)
+		{
+			if(i == pattern.length() - 1)
+  			{
+  				t[nextNode].patternEnd = true;
+  			}
+  			else
+  			{
+  				currentNode = nextNode;
+  			}
+		}
+		else
+		{
+			t[currentNode].next[currentIndex] = ++nodeNum;
+			Node temp;
+			if(i == pattern.length() - 1)
+  			{
+  				temp.patternEnd = true;
+  			}
+			t.push_back(temp);
+			currentNode = nodeNum;
+		}
+	  }
+  }
+  return t;
+}
+
+vector <int> solve (const string& text, int n, vector <string>& patterns)
+{
+	vector <int> result;
+	trie t = build_trie(patterns);
+	for (int i = 0; i < text.length(); i++)
+	{
+		string newText = text.substr(i, text.length() - i);
+		int containPattern = prefixTrieMatching(newText,t);
+		if (containPattern)
+		{
+			result.push_back(i);
+		}
+	}
+	return result;
+}
+
+int main (void)
+{
+	string t;
+	cin >> t;
+
+	int n;
+	cin >> n;
+
+	vector <string> patterns (n);
+	for (int i = 0; i < n; i++)
+	{
+		cin >> patterns[i];
+	}
+
+	vector <int> ans;
+	ans = solve (t, n, patterns);
+
+	for (int i = 0; i < (int) ans.size (); i++)
+	{
+		cout << ans[i];
+		if (i + 1 < (int) ans.size ())
+		{
+			cout << " ";
+		}
+		else
+		{
+			cout << endl;
+		}
+	}
+
+	return 0;
+}
